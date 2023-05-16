9c43b82752ca144821f2286e4189c5262089982f
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-05-16 15:18:30 +0900

    Add namespace

--- Assets/PolyhedronMeshGenerator/Scripts/MeshExporter.cs
+++ Assets/PolyhedronMeshGenerator/Scripts/MeshExporter.cs
@@ -3,58 +3,61 @@ using System.IO;
 using System.Text;
 using UnityEngine;
 
-public static class MeshExporter
+namespace PolyhedronMeshGenerator
 {
-    public static void Export(MeshFilter meshFilter)
-    {
-        string dirpath = Application.streamingAssetsPath + "/Mesh";
-        string dateTimeStr = DateTime.Now.ToString("yyyyMMddHHmmss");
-
-        byte[] serialized = SerializeAsObjFormat(meshFilter.sharedMesh.vertices, meshFilter.sharedMesh.triangles);
-		string filename = "Mesh_" + dateTimeStr + ".obj";
-        SaveByteData(dirpath, filename, serialized);
-
-    }
-
-    async static void SaveByteData(string dirpath, string filename, byte[] data)
-    {
-        if(!Directory.Exists(dirpath))
-        {
-            Directory.CreateDirectory(dirpath);
-        }
-
-        string filepath = dirpath + "/" + filename;
-
-        using(var fs = new FileStream(filepath, FileMode.OpenOrCreate))
-        {
-            await fs.WriteAsync(data, 0, data.Length);
-        }
-    }
-
-    static byte[] SerializeAsObjFormat(Vector3[] vertices, int[] triangles)
+	public static class MeshExporter
 	{
-		StringBuilder objTextBuilder = new StringBuilder();
-		int offset = 0;
-		int count = 0;
-
-		objTextBuilder.AppendFormat("o object.{0}\n", ++count);
-		foreach(var vertex in vertices)
+		public static void Export(MeshFilter meshFilter)
 		{
-			objTextBuilder.AppendFormat("v {0} {1} {2}\n", vertex.x, vertex.y, vertex.z);
+			string dirpath = Application.streamingAssetsPath + "/Mesh";
+			string dateTimeStr = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+			byte[] serialized = SerializeAsObjFormat(meshFilter.sharedMesh.vertices, meshFilter.sharedMesh.triangles);
+			string filename = "Mesh_" + dateTimeStr + ".obj";
+			SaveByteData(dirpath, filename, serialized);
+
 		}
 
-		objTextBuilder.Append("\n");
-
-		for (int i = 0; i < triangles.Length; i += 3)
+		async static void SaveByteData(string dirpath, string filename, byte[] data)
 		{
-			objTextBuilder.AppendFormat("f {0} {1} {2}\n",
-				triangles[i + 0] + 1 + offset,
-				triangles[i + 1] + 1 + offset,
-				triangles[i + 2] + 1 + offset);
+			if(!Directory.Exists(dirpath))
+			{
+				Directory.CreateDirectory(dirpath);
+			}
+
+			string filepath = dirpath + "/" + filename;
+
+			using(var fs = new FileStream(filepath, FileMode.OpenOrCreate))
+			{
+				await fs.WriteAsync(data, 0, data.Length);
+			}
 		}
 
-		objTextBuilder.Append("\n\n");
+		static byte[] SerializeAsObjFormat(Vector3[] vertices, int[] triangles)
+		{
+			StringBuilder objTextBuilder = new StringBuilder();
+			int offset = 0;
+			int count = 0;
 
-		return System.Text.Encoding.ASCII.GetBytes(objTextBuilder.ToString());
-	}
+			objTextBuilder.AppendFormat("o object.{0}\n", ++count);
+			foreach(var vertex in vertices)
+			{
+				objTextBuilder.AppendFormat("v {0} {1} {2}\n", vertex.x, vertex.y, vertex.z);
+			}
+
+			objTextBuilder.Append("\n");
+
+			for (int i = 0; i < triangles.Length; i += 3)
+			{
+				objTextBuilder.AppendFormat("f {0} {1} {2}\n",
+					triangles[i + 0] + 1 + offset,
+					triangles[i + 1] + 1 + offset,
+					triangles[i + 2] + 1 + offset);
+			}
+
+			objTextBuilder.Append("\n\n");
+
+			return System.Text.Encoding.ASCII.GetBytes(objTextBuilder.ToString());
+		}
+	}	
 }
--- Assets/PolyhedronMeshGenerator/Scripts/RegularTetrahedronMesh.cs
+++ Assets/PolyhedronMeshGenerator/Scripts/RegularTetrahedronMesh.cs
@@ -1,47 +1,50 @@
 ﻿using UnityEngine;
 
-public class RegularTetrahedronMesh : MonoBehaviour
+namespace PolyhedronMeshGenerator
 {
-    [SerializeField] bool exportMesh = false;
-    [SerializeField] Material material;
-
-    void Start()
+    public class RegularTetrahedronMesh : MonoBehaviour
     {
-        var filter = gameObject.AddComponent<MeshFilter>();
-        filter.sharedMesh = CreateRegularTetrahedronMesh(); 
+        [SerializeField] bool exportMesh = false;
+        [SerializeField] Material material;
 
-        var renderer = gameObject.AddComponent<MeshRenderer>();
-        renderer.sharedMaterial = material;
-
-        if (exportMesh)
+        void Start()
         {
-            MeshExporter.Export(filter);
+            var filter = gameObject.AddComponent<MeshFilter>();
+            filter.sharedMesh = CreateRegularTetrahedronMesh(); 
+
+            var renderer = gameObject.AddComponent<MeshRenderer>();
+            renderer.sharedMaterial = material;
+
+            if (exportMesh)
+            {
+                MeshExporter.Export(filter);
+            }
         }
-    }
 
-    Mesh CreateRegularTetrahedronMesh()
-    {
-        var mesh = new Mesh();
-
-        mesh.vertices = new Vector3[]
+        Mesh CreateRegularTetrahedronMesh()
         {
-            new Vector3(0f, -1f, 0f),
-            new Vector3(0f, 1f/3f, Mathf.Sqrt(8f)/3f),
-            new Vector3( Mathf.Sqrt(8f)/(2f*Mathf.Sqrt(3f)), 1f/3f, -Mathf.Sqrt(8f)/6f),
-            new Vector3(-Mathf.Sqrt(8f)/(2f*Mathf.Sqrt(3f)), 1f/3f, -Mathf.Sqrt(8f)/6f),
-        };
+            var mesh = new Mesh();
 
-        mesh.triangles = new int[]
-        {
-            0, 2, 1,
-            0, 1, 3,
-            0, 3, 2,
-            1, 2, 3,
-        };
+            mesh.vertices = new Vector3[]
+            {
+                new Vector3(0f, -1f, 0f),
+                new Vector3(0f, 1f/3f, Mathf.Sqrt(8f)/3f),
+                new Vector3( Mathf.Sqrt(8f)/(2f*Mathf.Sqrt(3f)), 1f/3f, -Mathf.Sqrt(8f)/6f),
+                new Vector3(-Mathf.Sqrt(8f)/(2f*Mathf.Sqrt(3f)), 1f/3f, -Mathf.Sqrt(8f)/6f),
+            };
 
-        mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
+            mesh.triangles = new int[]
+            {
+                0, 2, 1,
+                0, 1, 3,
+                0, 3, 2,
+                1, 2, 3,
+            };
 
-        return mesh;
+            mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+
+            return mesh;
+        }
     }
 }
